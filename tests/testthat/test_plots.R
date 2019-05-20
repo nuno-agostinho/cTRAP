869b31018d31f88865a8512ad1c0dd16d34f01e0
Nuno Agostinho <nunodanielagostinho@gmail.com>
2019-05-20 17:50:07 +0100

    Test plotting non-ranked perturbations

--- tests/testthat/test_plots.R
+++ tests/testthat/test_plots.R
@@ -1,30 +1,31 @@
 context("Plot results")
 
-# data(compareKnockdown)
 data("cmapPerturbationsCompounds")
 perturbations <- cmapPerturbationsCompounds
 data("diffExprStat")
 
-compareKnockdown <- compareAgainstCMap(diffExprStat, perturbations)
-condition <- compareKnockdown[1, 1]
+cmp <- compareAgainstCMap(diffExprStat, perturbations)
+condition <- cmp[1, 1]
+
+# Plot cmapComparison object ---------------------------------------------------
 
 test_that("Plot similar perturbations according to selected method", {
-    plot <- plot(compareKnockdown, method="pearson")
+    plot <- plot(cmp, method="pearson")
     expect_s3_class(plot, "ggplot")
     expect_match(plot$labels$y, "Pearson")
 
-    plot <- plot(compareKnockdown, method="spearman")
+    plot <- plot(cmp, method="spearman")
     expect_s3_class(plot, "ggplot")
     expect_match(plot$labels$y, "Spearman")
 
-    plot <- plot(compareKnockdown, method="gsea")
+    plot <- plot(cmp, method="gsea")
     expect_s3_class(plot, "ggplot")
     expect_match(plot$labels$y, "WCTS")
 
     # Avoid plotting based on methods not available in the cmapComparison object
-    compareKnockdownPearson <- compareAgainstCMap(diffExprStat, perturbations,
-                                                  method="pearson")
-    expect_error(plot(compareKnockdownPearson, method="gsea"))
+    cmpPearson <- compareAgainstCMap(diffExprStat, perturbations,
+                                     method="pearson")
+    expect_error(plot(cmpPearson, method="gsea"))
 })
 
 test_that("Plot custom top and bottom similar perturbations", {
@@ -34,18 +35,18 @@ test_that("Plot custom top and bottom similar perturbations", {
         expect_s3_class(plot, "ggplot")
         expect_equal(length(unique(plot$data$label)) - 1, sum(top, bottom))
     }
-    plotTopBottomSimilarPerts(compareKnockdown, 7, 0)
+    plotTopBottomSimilarPerts(cmp, 7, 0)
 
     # Bottom 4 least similar perturbations
-    plotTopBottomSimilarPerts(compareKnockdown, 0, 4)
+    plotTopBottomSimilarPerts(cmp, 0, 4)
 
     # Top 5 most and bottom 2 least similar perturbations
-    plotTopBottomSimilarPerts(compareKnockdown, 5, 2)
+    plotTopBottomSimilarPerts(cmp, 5, 2)
 })
 
 test_that("Plot similar perturbations with/without metadata", {
-    plot1 <- plot(compareKnockdown, showMetadata=TRUE)
-    plot2 <- plot(compareKnockdown, showMetadata=FALSE)
+    plot1 <- plot(cmp, showMetadata=TRUE)
+    plot2 <- plot(cmp, showMetadata=FALSE)
 
     expect_s3_class(plot1, "ggplot")
     expect_s3_class(plot2, "ggplot")
@@ -54,6 +55,21 @@ test_that("Plot similar perturbations with/without metadata", {
     originalLabel <- unique(plot2$data$label)
     expect_false(all(metadataLabel != originalLabel))
 })
+
+test_that("Plot only ranked perturbations", {
+    plot <- plot(cmp, "spearman", plotNonRankedPerturbations=FALSE)
+    expect_s3_class(plot, "ggplot")
+    expect_false(plot$guides$colour)
+})
+
+test_that("Plot non-ranked perturbations", {
+    plot <- plot(cmp, "spearman", plotNonRankedPerturbations=TRUE)
+    expect_s3_class(plot, "ggplot")
+    expect_identical(plot$labels$colour, "ranked")
+    expect_null(plot$guides$colour)
+})
+
+# Plot cmapPerturbations object ------------------------------------------------
 
 test_that("Plot Spearman's correlation for a single perturbation", {
     plot <- plot(perturbations, colnames(perturbations)[[1]], diffExprStat,
