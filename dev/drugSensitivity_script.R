093c1b9fd73d7a5bc83fc02d1af4548ac3fa1560
Nuno Agostinho <nunodanielagostinho@gmail.com>
2020-11-18 12:51:55 +0000

    Fix drug sensitivity script

--- dev/drugSensitivity_script.R
+++ dev/drugSensitivity_script.R
@@ -2,13 +2,13 @@
 # genomicsDrugSensitivityCorCTRP <- prepareGenomicsDrugSensitivityAssociation("CTRP")
 # genomicsDrugSensitivityCorNCI60 <- prepareGenomicsDrugSensitivityAssociation("NCI60")
 
-ctrp  <- loadGenomicsDrugSensitivityAssociation("CTRP")
-gdsc  <- loadGenomicsDrugSensitivityAssociation("GDSC")
-nci60 <- loadGenomicsDrugSensitivityAssociation("NCI60")
+ctrp  <- prepareExpressionDrugSensitivityAssociation("CTRP")
+gdsc  <- prepareExpressionDrugSensitivityAssociation("GDSC")
+nci60 <- prepareExpressionDrugSensitivityAssociation("NCI60")
 
-corCTRP  <- predictTargetingDrug(diffExprStat, ctrp)
-corGDSC  <- predictTargetingDrug(diffExprStat, gdsc)
-corNCI60 <- predictTargetingDrug(diffExprStat, nci60)
+corCTRP  <- predictTargetingDrugs(diffExprStat, ctrp)
+corGDSC  <- predictTargetingDrugs(diffExprStat, gdsc)
+corNCI60 <- predictTargetingDrugs(diffExprStat, nci60)
 
 # Plot CMap perturbation similarity with drug sensitivity results --------------
 getCMapResults <- function(diffExprStat, ..., subset=FALSE) {
@@ -51,32 +51,4 @@ drugSets <- prepareDrugSets(descriptors2D, "PubChem_SID")
 
 descriptors3D <- data.table::fread("compound_descriptors_3D.csv",
                                    na.strings=c("", "NA"))
-
-# analyseDrugSetEnrichment <- function(stats, sets) {
-#     if (is(stats, "similarPerturbations")) {
-#         # Use negative values to invert perturbation relevance
-#         rank   <- -cmp[["rankProduct_rank"]]
-#         values <- na.omit(setNames(rank, cmp[[1]])) # Discard missing values
-#
-#         metadata     <- attr(stats, "metadata")
-#         compoundInfo <- attr(stats, "compoundInfo")
-#
-#         # Convert to perturbation name (discard if non-matching)
-#         names(values) <- metadata[
-#             metadata$sig_id == names(values), ][["pert_iname"]]
-#         values <- values[!is.na(names(values))]
-#
-#         # Convert to PubChem identifier
-#         names(values) <- compoundInfo[
-#             match(names(values), compoundInfo$pert_iname), ][["pubchem_cid"]]
-#
-#         values <- values[!is.na(names(values))]
-#         dt <- data.table(PubChem=names(values), rank=values)[
-#             , .("rank"=max(rank)), by=PubChem]
-#         stats <- setNames(dt$rank, dt$PubChem)
-#     }
-#     stats <- sort(stats)
-#     browser()
-#     fgsea(sets, stats, nperm=10000, maxSize=500)
-# }
 # analyseDrugSetEnrichment(cmapResults, drugSets)
